0fdeb3adcb12e8c95f5c2dfd88b1b30f60d12565
kunalonly <kunalraipurkar31@gmail.com>
2022-10-11 22:36:33 +0530

    commit after objects

---
+++ practise2.js
@@ -0,0 +1,10 @@
+let doller=[1,5,10,500]
+
+let cent=[];
+
+for(let i=0;i<doller.length;i++)
+{
+    cent.push((doller[i]*100));
+}
+
+console.log(cent)
---
+++ practise3.js
@@ -0,0 +1,41 @@
+let users=[{
+    username:'kunal',
+    email:'kunalraipurkar31@gamil.com',
+    password:'Kunal@123',
+    userSubcriptionStatus:'VIP',
+    userLessionComplited:[0,1,2]
+},
+{
+    username:'kunal1',
+    email:'kunalraipurkar3@gamil.com',
+    userSubcriptionStatus:'VIP',
+    userLessionComplited:[0,1,2]
+}];
+
+
+function login(email,password)
+{
+    for(let i=0;i<users.length;i++)
+    {
+    if(users[i].email===email)
+    {
+    console.log("You are present in the database please provide password for login")
+
+    if (users[i].password===password)
+    {
+        console.log("You logged in Seccessfully");
+    }
+
+    else
+    {
+    console.log("Please provide Correct Passwoord");
+    }
+
+    return;
+}
+    
+    }
+    console.log("Could Not find This user");
+    }
+
+login("kunalraipurkar31@gamil.com","Kunal@13")
