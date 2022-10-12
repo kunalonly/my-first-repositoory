de9d6da616f2ae9d7d8cac9f20967f782a6e1382
kunalonly <kunalraipurkar31@gmail.com>
2022-10-12 10:03:19 +0530

    Creating promise

--- promise.js
+++ promise.js
@@ -1,10 +1,38 @@
 const emailRef = document.querySelector(".email");
 
-fetch("https://jsonplaceholder.typicode.com/users/1")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    emailRef.innerHTML = data.email;
-  });
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+//   .then((response) => {
+//     return response.json();
+//   })
+//  .then((data) => {
+//     console.log(data);
+//     emailRef.innerHTML = data.address;
+//   });
+
+// async function main()
+// {
+//     const response=await fetch("https://jsonplaceholder.typicode.com/users/1");
+//    const data= await(response.json());
+//    emailRef.innerHTML=data.name;
+
+// }
+
+
+function getSubcriptionStatus()
+{
+    return new Promise ((resolve,reject)=>{
+        resolve('VIP')
+
+    })
+}
+
+getSubcriptionStatus().then(response=>console.log(response));
+
+async function main()
+{
+   const data=await getSubcriptionStatus();
+
+   emailRef.innerHTML=data
+}
+main();
+
